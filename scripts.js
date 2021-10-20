2dab7b19734384986d0274ee963126d74fac36ed
Eric Parks <eparks1860@gmail.com>
2021-10-19 21:34:37 -0400

    Fixed a bug with game starting in test state

--- scripts.js
+++ scripts.js
@@ -21,14 +21,15 @@ const gameBoard = (() => {
     //const board = [];
 
     //temporary for testing
-    let board = ['x', 'o','','x','','','o','',''];
+    //let board = ['x', 'o','','x','','','o','',''];
+    let board = ["", "", "", "", "", "", "", "", ""];
 
     let currentMarksTurn = 'x';
     let player1Mark = '';
     let player2Mark = '';
 
     const startGame = (player1, player2) => {
-        console.log(player1)
+        //console.log(player1)
         player1Mark = player1.getMark();
         player2Mark = player2.getMark();
 
@@ -47,9 +48,11 @@ const gameBoard = (() => {
             displayController.displayBoard(board);
 
             //Check for a winner
-            console.log(`Winner is:${checkForWinner()}`)
+            
 
             if (checkForWinner()) {
+                //console.log(`Winner is:${checkForWinner()}`)
+
                 //End the game
                 clearBoard();
                 displayController.displayBoard(board);
@@ -62,14 +65,14 @@ const gameBoard = (() => {
 
     const checkForWinner = () => {
         //Winning Patterns
-        if (board[0] === board[1] && board[1] === board[2] 
-            || board[3] === board[4] && board[4] === board[5]
-            || board[6] === board[7] && board[7] === board[8]
-            || board[0] === board[3] && board[3] === board[6]
-            || board[1] === board[4] && board[4] === board[7]
-            || board[2] === board[5] && board[5] === board[8]
-            || board[0] === board[4] && board[4] === board[8]
-            || board[2] === board[4] && board[4] === board[6]) {
+        if (board[0] === board[1] && board[1] === board[2] && board[2] !== ""
+            || board[3] === board[4] && board[4] === board[5] && board[5] !== ""
+            || board[6] === board[7] && board[7] === board[8] && board[8] !== ""
+            || board[0] === board[3] && board[3] === board[6] && board[6] !== ""
+            || board[1] === board[4] && board[4] === board[7] && board[7] !== ""
+            || board[2] === board[5] && board[5] === board[8] && board[8] !== ""
+            || board[0] === board[4] && board[4] === board[8] && board[8] !==""
+            || board[2] === board[4] && board[4] === board[6] && board[6] !== "") {
                 return true;
             }
             
